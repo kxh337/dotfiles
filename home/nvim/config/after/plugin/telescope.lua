f2ce9323e8dc2453b8d1858b1686d9ea28d39c99
Kei <khitomi337@gmail.com>
2025-12-22 15:04:34 -0800

    Fixed the mappings

--- home/nvim/config/after/plugin/telescope.lua
+++ home/nvim/config/after/plugin/telescope.lua
@@ -3,16 +3,10 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>P', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>p', builtin.git_files, { desc = 'Telescope find git files' })
 vim.keymap.set('n', '<leader>G', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>g', builtin.grep_string, { desc = 'Telescope live grep cword' })
+vim.keymap.set('n', '<leader>gg', builtin.grep_string, { desc = 'Telescope live grep cword' })
 vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>h', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- LSP searches
-vim.keymap.set('n', '<leader>gr', builtin.lsp_references, 
-               { 
-								 desc = 'Telescope LSP refrerences',
-								 include_declaration = true,
-								 include_current_line = true,
-							 }
-)
-vim.keymap.set('n', '<leader>gr', builtin.diagnostics, { desc = 'Telescope iagnostic lists'})
+vim.keymap.set('n', '<leader>gr', builtin.lsp_references, { desc = 'Telescope LSP refrerences'})
+vim.keymap.set('n', '<leader>gd', builtin.diagnostics, { desc = 'Telescope iagnostic lists'})
