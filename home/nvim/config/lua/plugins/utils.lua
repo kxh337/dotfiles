0905e403035e6678661eb6bfe9cb3df6256ce766
Kei <khitomi337@gmail.com>
2025-12-31 08:45:55 -0800

    Added keymaps for autocomplete

--- home/nvim/config/lua/plugins/utils.lua
+++ home/nvim/config/lua/plugins/utils.lua
@@ -49,7 +49,11 @@ return {
     -- C-k: Toggle signature help (if signature.enabled = true)
     --
     -- See :h blink-cmp-config-keymap for defining your own keymap
-    keymap = { preset = 'default' },
+    keymap = {
+      preset = 'enter',
+      ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+      ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+    },
 
     appearance = {
       -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
